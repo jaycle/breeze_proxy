6ff22c0978d1fe2107e6dfa34cc8ac54af4ea4f4
Jayson Cleary <jayson.cleary@gmail.com>
2019-01-06 01:47:28 -0500

    Remove restplus for simple flask method
    Remove restplus and use flasgger for swagger ui.

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from flask.logging import default_handler
-from .apis.api import blueprint as api
+from .apis.api import api
+from flasgger import Swagger
 import logging
 
 
 app = Flask(__name__)
+swagger = Swagger(app)
 app.register_blueprint(api, url_prefix='/api')
 logger = logging.getLogger(__name__)
 logger.addHandler(default_handler)
 
 @app.route('/')
 def root():
-    return 'An API for interacting with the Breeze CMS. Head to <a href="api/">api/</a> for Swagger docs.'
+    return 'An API for interacting with the Breeze CMS. Head to <a href="apidocs/">apidocs/</a> for Swagger docs.'
